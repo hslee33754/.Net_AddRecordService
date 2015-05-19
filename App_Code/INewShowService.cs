0f18ba85304bf18dcc92d7eda85f3650aa1e57a8
Kate Lee <hslee.33754@gmail.com>
2015-05-19 12:42:36 -0700

    Add Show Service for ShowTracker DB

---
+++ App_Code/INewShowService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "INewShowService" in both code and config file together.
+[ServiceContract]
+public interface INewShowService
+{
+    [OperationContract]
+    bool AddArtist(Artist a);
+
+    [OperationContract]
+    bool AddShow(ShowLite s, string artist);
+
+    [OperationContract]
+    List<string> GetArtists();
+
+    [OperationContract]
+    List<string> GetVenue();
+
+}
+
+[DataContract]
+public class ShowLite
+{
+    [DataMember]
+    public string ShowName { set; get; }
+
+    [DataMember]
+    public DateTime ShowDate { set; get; }
+
+    [DataMember]
+    public TimeSpan ShowTime { set; get; }
+
+    [DataMember]
+    public string ShowTicketinfo { set; get; }
+
+    [DataMember]
+    public DateTime ShowDateEntered { set; get; }
+
+    [DataMember]
+    public TimeSpan ShowDetailArtistStartTime { set; get; }
+
+    [DataMember]
+    public string ShowDetailAdditional { set; get; }
+
+    [DataMember]
+    public Int32 ArtistKey { set; get; }
+
+}
---
+++ App_Code/NewShowService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "NewShowService" in code, svc and config file together.
+public class NewShowService : INewShowService
+{
+    ShowTrackerEntities ste = new ShowTrackerEntities();
+
+    public bool AddArtist(Artist a)
+    {
+        bool result = true;
+
+        try
+        {
+            Artist arts = new Artist();
+            arts.ArtistName = a.ArtistName;
+            arts.ArtistEmail = a.ArtistEmail;
+            arts.ArtistWebPage = a.ArtistWebPage;
+            arts.ArtistDateEntered = DateTime.Now;
+
+            ste.Artists.Add(arts);
+            ste.SaveChanges();
+        }
+        catch
+        {
+            result = false;
+        }
+        return result;
+    }
+
+    public bool AddShow(ShowLite s, string artist)
+    {
+        bool result = true;
+
+        try
+        {
+            Show sh = new Show();
+            sh.ShowName = s.ShowName;
+            sh.ShowDate = s.ShowDate;
+            sh.ShowTime = s.ShowTime;
+            sh.ShowTicketInfo = s.ShowTicketinfo;
+            sh.ShowDateEntered = DateTime.Now;
+
+            ste.Shows.Add(sh);
+
+            ShowDetail shdt = new ShowDetail();
+            //Artist arts = ste.Artists.FirstOrDefault(a => a.ArtistName == artist);   
+            shdt.ArtistKey = s.ArtistKey;
+
+            shdt.Show = sh;
+            shdt.ShowDetailAdditional = s.ShowDetailAdditional;
+            shdt.ShowDetailArtistStartTime = s.ShowDetailArtistStartTime;
+
+            ste.ShowDetails.Add(shdt);
+            ste.SaveChanges();
+
+        }
+        catch
+        {
+            result = false;
+        }
+        return result;
+    }
+
+    public List<string> GetArtists()
+    {
+        var arts = from a in ste.Artists
+                   orderby a.ArtistName
+                   select new { a.ArtistKey, a.ArtistName };
+        List<string> artists = new List<string>();
+        foreach (var a in arts)
+        {
+            artists.Add(a.ArtistName);
+        }
+        return artists;
+    }
+
+    public List<string> GetVenue()
+    {
+        var vne = from v in ste.Venues
+                  orderby v.VenueName
+                  select new { v.VenueKey, v.VenueName };
+        List<string> venues = new List<string>();
+        foreach (var v in vne)
+        {
+            venues.Add(v.VenueName);
+        }
+        return venues;
+    }
+
+}
